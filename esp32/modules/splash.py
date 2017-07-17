9fc009153343308e763a0eaa5511e121afa4e48d
Arnout Engelen <arnouten@bzzt.net>
2017-07-18 01:00:55 +0200

    Clean up whitespace

--- esp32/modules/splash.py
+++ esp32/modules/splash.py
@@ -59,7 +59,7 @@ def draw_services():
                 y = y - abs(space_used)
         except BaseException as msg:
             print("[SPLASH] Service draw exception: ", msg)
-        
+
 # RTC
 
 def clockstring():
@@ -75,7 +75,7 @@ def clockstring():
       hourstr = "0"+hourstr
     minstr = str(min)
     if (min<10):
-      minstr = "0"+minstr 
+      minstr = "0"+minstr
     return daystr+"-"+monthstr+"-"+str(year)+" "+hourstr+":"+minstr
 
 def disableWifi():
@@ -85,9 +85,9 @@ def disableWifi():
 def getTimeNTP():
     import ntp
     import wifi
-    
+
     current_datetime = time.time()
-    
+
     if (current_datetime<1482192000): #If clock on time before 2017
         draw_msg("Configuring clock...", "Welcome!")
         wifi.init()
@@ -112,7 +112,7 @@ def getTimeNTP():
     #else:
     #    print("[SPLASH] RTC already set.")
     return True
-    
+
 # BATTERY
 
 def battery_percent_internal(vempty, vfull, vbatt):
@@ -138,7 +138,7 @@ def draw_msg(title, desc):
     ugfx.string(0, 25, desc, "Roboto_Regular12", ugfx.BLACK)
     ugfx.set_lut(ugfx.LUT_FASTER)
     ugfx.flush()
-    
+
 def draw_dialog(title, desc, msg):
     ugfx.clear(ugfx.WHITE)
     ugfx.string(0, 0, title, "PermanentMarker22", ugfx.BLACK)
@@ -154,7 +154,7 @@ def draw_logo(x,y,h):
     ugfx.line(x, y + 47, x + 14 + len, y + 47, ugfx.BLACK)
     ugfx.line(x + 10 + len, y + 27, x + 10 + len, y + 45, ugfx.BLACK)
     ugfx.string(x + 10, y + 50,"Anyway","Roboto_BlackItalic24",ugfx.BLACK)
-    
+
 def draw_helper_clear(full):
     if full:
         ugfx.clear(ugfx.BLACK)
@@ -162,7 +162,7 @@ def draw_helper_clear(full):
     ugfx.clear(ugfx.WHITE)
     if full:
         ugfx.flush()
-    
+
 def draw_helper_battery(percent,cstate):
     ugfx.area(2,2,40,18,ugfx.WHITE)
     ugfx.box(42,7,2,8,ugfx.WHITE)
@@ -177,29 +177,29 @@ def draw_helper_battery(percent,cstate):
                 ugfx.string(5,5,"empty","Roboto_Regular12",ugfx.BLACK)
     else:
         ugfx.string(2,5,"no batt","Roboto_Regular12",ugfx.BLACK)
-    
+
 def draw_helper_header(text):
     ugfx.area(0,0,ugfx.width(),23,ugfx.BLACK)
     ugfx.string(45, 1, text,"DejaVuSans20",ugfx.WHITE)
-    
+
 def draw_helper_footer(text_l, text_r):
     ugfx.string(0, ugfx.height()-13, text_l, "Roboto_Regular12",ugfx.BLACK)
     l = ugfx.get_string_width(text_r,"Roboto_Regular12")
     ugfx.string(ugfx.width()-l, ugfx.height()-13, text_r, "Roboto_Regular12",ugfx.BLACK)
-    
+
 def draw_helper_nick(default):
     nick = badge.nvs_get_str("owner", "name", default)
     ugfx.string(0, 40, nick, "PermanentMarker36", ugfx.BLACK)
     htext = badge.nvs_get_str("owner", "htext", "")
     if (htext!=""):
       draw_logo(160, 25, htext)
-    
+
 def draw_helper_flush(full):
     if (full):
         ugfx.set_lut(ugfx.LUT_FULL)
     ugfx.flush()
     ugfx.set_lut(ugfx.LUT_FASTER)
-    
+
 def draw_home(percent, cstate, status, full_clear, going_to_sleep):
     draw_helper_clear(full_clear)
     if (cstate):
@@ -215,7 +215,7 @@ def draw_home(percent, cstate, status, full_clear, going_to_sleep):
     draw_helper_nick("Unknown")
     draw_services()
     draw_helper_flush(True)
-    
+
 def draw_batterylow(percent):
     draw_helper_clear(True)
     draw_helper_header("")
@@ -223,28 +223,28 @@ def draw_batterylow(percent):
     draw_helper_footer("BATTERY LOW, CONNECT CHARGER!", "")
     draw_helper_nick(":(           Zzzz...")
     draw_helper_flush(False)
-    
+
 # START LAUNCHER
-    
+
 def start_launcher(pushed):
     #print("START LAUNCHER: ", pushed)
     if(pushed):
         global splashTimer
         splashTimer.deinit()
         appglue.start_app("launcher")
-      
+
 # SLEEP
-            
+
 def badge_sleep():
     print("[SPLASH] Going to sleep now...")
     badge.eink_busy_wait() #Always wait for e-ink
     deepsleep.start_sleeping(30000) #Sleep for 30 seconds
-    
+
 def badge_sleep_forever():
     print("[SPLASH] Going to sleep WITHOUT TIME WAKEUP now...")
     badge.eink_busy_wait() #Always wait for e-ink
     deepsleep.start_sleeping(0) #Sleep until button interrupt occurs
-    
+
 # TIMER
 
 def splashTimer_callback(tmr):
@@ -268,19 +268,19 @@ def splashTimer_callback(tmr):
         if (loop_services(loopCnt)):
             loopCnt = 0
     loopCnt = loopCnt + 1
-  
+
 def start_sleep_counter():
     global splashTimer
     splashTimer.init(period=500, mode=machine.Timer.PERIODIC, callback=splashTimer_callback)
-  
+
 def stop_sleep_counter():
     global splashTimer
     splashTimer.deinit()
-    
+
 def restart_sleep_counter():
     stop_sleep_counter()
     start_sleep_counter()
-    
+
 # WELCOME (SETUP, SPONSORS OR CLOCK)
 
 def welcome():
@@ -297,8 +297,8 @@ def welcome():
         getTimeNTP() # Set clock if needed
 
 # MAIN
-  
-def splash_main():   
+
+def splash_main():
     cstate = badge.battery_charge_status()
     percent = battery_percent()
     vbatt = badge.battery_volt_sense()
@@ -313,13 +313,13 @@ def splash_main():
         setup_services()
         start_sleep_counter()
     else:
-        draw_batterylow(percent)       
+        draw_batterylow(percent)
         badge_sleep_forever()
-  
+
 #Global variables
 splashTimer = machine.Timer(0)
 services = []
 loopCnt = 0
 
 splash_main()
- 
+
